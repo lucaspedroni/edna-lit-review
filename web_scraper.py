57de4ffe64849b86563fb3b8435c2a5b5ba7e5aa
Kirk Gasper <wigasper@gmail.com>
2019-10-30 15:00:31 -0500

    .

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 import re
 import sys
+import time
 import logging
 import traceback
+import configparser
+import urllib.robotparser
+from urllib.parse import urlparse
 
 import requests
 import scholarly
 from bs4 import BeautifulSoup
-    
+
+# TODO: returning disallowed for some domains that should be allowed, see logs
+def robots_allowed(url, checked_netlocs, logger):
+    # extract base domain
+    netloc = urlparse(url).netloc
+
+    if netloc not in checked_netlocs:
+        robot_parser = urllib.robotparser.RobotFileParser()
+        robot_parser.set_url(f"http://{netloc}/robots.txt")
+        robot_parser.read()
+
+        if robot_parser.can_fetch("*", url):
+            checked_netlocs.append(netloc)
+            return True
+        else:
+            logger.info(f"Disallowed from {netloc}, {url}")
+            return False
+    else:
+        return True
+
 def extract_natural_text(soup):
         text = [" ".join([str(it).strip() for it in item.contents]) for item in soup]
         text = [remove_tags(item) for item in text if item and item != " " 
@@ -44,14 +67,22 @@ if __name__ == "__main__":
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-        
+    
+    # Get User-Agent data
+    config = configparser.ConfigParser()
+    config.read("scraper.cfg")
+
+    checked_netlocs = []
+
     valid_text = re.compile(r"[Ee]nvironmental|DNA")
     
     headers = requests.utils.default_headers()
-    headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+    headers["User-Agent"] = config.get("scraper", "user_agent")
     
     query = scholarly.search_pubs_query("environmental DNA")
     
+    # Check timing of requests
+    last_req = time.perf_counter()
 #    texts = []
 #    authors = []
 #    titles = []
@@ -59,19 +90,28 @@ if __name__ == "__main__":
     with open("gs_scrape_results", "w") as out:
         for query_result in query:
             try:
+                if time.perf_counter() - last_req < 1:
+                    time.sleep(1)
+
                 title = query_result.bib["title"]
                 author = query_result.bib["author"] 
                 cit_count = query_result.citedby
-                
-                result = requests.get(query_result.bib['url'], headers=headers)
-                
-                soup = BeautifulSoup(result.content)
-                text = extract_natural_text(soup.find_all("p"))
-                
-                # more validation here:
-                if valid_text.search(text):
-                    out.write("\t".join([author, title, cit_count, text]))
-                    out.write("\n")
+                url = query_result.bib['url']
+
+                if robots_allowed(url, checked_netlocs, logger):
+                    result = requests.get(url, headers=headers)
+                    
+                    soup = BeautifulSoup(result.content, features="html5lib")
+                    text = extract_natural_text(soup.find_all("p"))
+                    
+                    # more validation here:
+                    if valid_text.search(text):
+                        out.write("\t".join([author, title, str(cit_count), text]))
+                        out.write("\n")
+
+                    last_req = time.perf_counter()
+            except ConnectionResetError as e:
+                logger.info(f"ConnectionResetError at {url}")
             except Exception as e:
                 trace = traceback.format_exc()
                 logger.error(repr(e))
